37d160da70d7bde38d7b2a03a8c2fd406909b477
Michael Palm <mischa@localhost.localdomain>
2015-05-30 18:14:39 +0200

    && -> ;

--- overwrite_conf.sh
+++ overwrite_conf.sh
@@ -3,7 +3,7 @@
 export CONF_SRC=`pwd`
 cd ~
 
-mv .bashrc .bashrc.original && ln -fs dotfiles/.bashrc .bashrc
-mv .bash_aliases .bash_aliases.original && ln -fs dotfiles/.bash_aliases .bash_aliases
-mv .vim .vim.original && ln -fs dotfiles/.vim .vim
-mv .vimrc .vimrc.original && ln -fs dotfiles/.vimrc .vimrc
+mv .bashrc .bashrc.original; ln -fs dotfiles/.bashrc .bashrc
+mv .bash_aliases .bash_aliases.original; ln -fs dotfiles/.bash_aliases .bash_aliases
+mv .vim .vim.original; ln -fs dotfiles/.vim .vim
+mv .vimrc .vimrc.original; ln -fs dotfiles/.vimrc .vimrc
