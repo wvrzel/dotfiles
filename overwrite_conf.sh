e52fd7fbe5f9d61f83a1158b742b1ea743e41496
root <root@mlaptop.network>
2016-07-19 14:08:10 +0200

    improved overwrite.conf.sh

--- overwrite_conf.sh
+++ overwrite_conf.sh
@@ -3,12 +3,29 @@
 export CONF_SRC=`pwd`
 cd ~
 
-mv .bashrc .bashrc.original; ln -fs dotfiles/.bashrc .bashrc
-mv .bash_aliases .bash_aliases.original; ln -fs dotfiles/.bash_aliases .bash_aliases
-mv .gitconfig .gitconfig.original; ln -fs dotfiles/.gitconfig .gitconfig
-mv .gitignore .gitignore.original; ln -fs dotfiles/.gitignore .gitignore
-mv .gitignore .gitignore.original; ln -fs dotfiles/.gitignore .gitignore
-mv .taskrc .taskrc.original; ln -fs dotfiles/.taskrc .taskrc
-mv .vim .vim.original; ln -fs dotfiles/.vim .vim
-mv .vimrc .vimrc.original; ln -fs dotfiles/.vimrc .vimrc
-mv .zshrc .zshrc.original; ln -fs dotfiles/.zshrc .zshrc
+timestamp=$(date +%s)
+for f in .bashrc .bash_aliases .gitconfig .gitignore .gitignore .taskrc .vim .vimrc .zshrc
+do
+    dst=$(readlink $f)
+    if [ -e $f -a "$dst" != "dotfiles/$f" ]
+    then
+        echo $dst,$f
+        mv $f $f.$timestamp.backup
+    fi
+    ln -fs dotfiles/$f $f
+done
+
+if [ ! -e .vim/bundle/Vundle.vim ]
+then
+    mkdir .vim/bundle
+    mkdir .vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim .vim/bundle/Vundle.vim
+fi
+
+grep "[user]" .gitconfig > /dev/null || (
+    echo >> .gitconfig
+    echo "[user]" >> .gitconfig
+    echo "    name = " >> .gitconfig
+    echo "    email = " >> .gitconfig
+    )
+vim +PluginUpdate .gitconfig
